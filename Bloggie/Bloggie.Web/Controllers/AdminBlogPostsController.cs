707b349a08ac16d654e2c5ded61af1f6a536f790
dedatekk <dedatekk@gmail.com>
2023-05-25 16:43:42 +1000

    32 can make a blogpost

--- Bloggie/Bloggie.Web/Controllers/AdminBlogPostsController.cs
+++ Bloggie/Bloggie.Web/Controllers/AdminBlogPostsController.cs
@@ -1,14 +1,36 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bloggie.Web.Models.ViewModels;
+using Bloggie.Web.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Bloggie.Web.Controllers
 {
     public class AdminBlogPostsController : Controller
     {
+        private readonly ITagRepository tagRepository;
+
+        public AdminBlogPostsController(ITagRepository tagRepository) {
+        this.tagRepository = tagRepository; 
+        }
 
         [HttpGet]
-        public IActionResult Add()
+        public async Task<IActionResult> Add()
         {
-            return View();
+            //get tags from repository
+            var tags = await tagRepository.GetAllAsync();
+
+            var model = new AddBlogPostRequest
+            {
+                Tags = tags.Select(x => new SelectListItem { Text = x.Name, Value = x.Id.ToString() })
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddBlogPostRequest addBlogPostRequest)
+        {
+            return RedirectToAction("Add");
         }
     }
 }
